6ee7c44740a944e83abe3e73d4631f86b951f18a
Frederico Vaz <FredVaz@MBPdeFrederico.deec.uc.pt>
2015-03-16 23:20:22 +0000

    update ex1

--- Processes - Lesson 2/ex1.c
+++ Processes - Lesson 2/ex1.c
@@ -1,7 +1,18 @@
-#include <sys/types.h> /* pid_t */
-#include <stdio.h>     /* printf, perror */
-#include <stdlib.h>    /* exit */
-#include <unistd.h>    /* _exit, fork */
+/*
+===============================================================================
+SO - Lesson 2 - Exercise 1
+
+Implementation of fork() The obtained result is as expected? Why?
+
++info: http://en.wikipedia.org/wiki/Fork_(system_call)
++info: http://en.wikipedia.org/wiki/Exit_(system_call)
+
+Developer: Frederico Vaz
+===============================================================================
+*/
+#include <stdio.h>     
+#include <stdlib.h>    
+#include <unistd.h> 
 
  
 int main(){
@@ -10,26 +21,26 @@ int main(){
 
   pid_t pid = fork(); // create a child process
 
-  if (pid == 0){ //quando fork() retorna 0 nós estamos no processo child (copiado)
-  	// Code only executed by child process
-
-  	printf("Eu sou o filho!\n");
+  if (pid == 0){ //quando fork() return 0 we are in process child
   	
-  }else {
+    // Code only executed by child process
+    for(int i = 1; i <= 100; i++)
+      printf("I am the child process!\n");
+    exit(EXIT_SUCCESS);
+  	
+  }else if(pid == -1){
+
+    printf("Failed to create child process!\n");
+
+  }else{
     // When fork() returns a positive number, we are in the parent process
     // and the return value is the PID of the newly created child process.
-  	// Code only executed by parent process
-
-  	printf("Eu sou o pai!\n");
-
+    // Code only executed by parent process
+    for(int i = 1; i <= 100; i++)
+      printf("I am the father process!\n");
   }
 
    // Code executed by both parent and child.
 
-
   return EXIT_SUCCESS;
 }
-
-//Nota: Ele cria o child onde pid recebe o PID do processo child logo é != 0 
-//pelo que executa "eu sou o pai", só depois de fork() retornar 0 é que entramos
-//no processo child e ai ele executa "eu sou o filho"
